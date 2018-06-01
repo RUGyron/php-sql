d24ed4b38fe58c57d94fd2745aee2efe662aa29c
RUGyron <32088165+RUGyron@users.noreply.github.com>
2018-06-02 00:41:41 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,10 +3,7 @@
 include "connecting.php";
 
 $id_worker = $_GET['id_worker'];
-
 $q = "DELETE from Worker WHERE id_worker = '$id_worker';";
-$q1 = "delete from Education where id_education = 2;";
-
 $res = mysqli_query($link, $q) or die('Ошибка запроса ('.mysqli_error($q).'): '.$q);
 if($res)
 {
